672c3a2f21fbbad74456866bd1f6a8a0942e3cfb
CodeWizardMyke <maikewilliam.dev@gmail.com>
2023-10-06 10:22:47 -0300

    resolvendo bug da transaction list

--- public/javascripts/transactions_list.js
+++ public/javascripts/transactions_list.js
@@ -3,11 +3,6 @@ async function shootGetTransactions(){
     handdlerPromisses(get_transactions[0], get_transactions[1], 'GET_TRANSACTIONS')
 }
 
-async function shootGetCategorys(){
-    const get_categotys = await getCategorys()
-    handdlerPromisses(get_categotys[0], get_categotys[1], 'GET_CATEGORYS')
-}
-
 async function shootGetTransactionByType(type){
     const get_transactions = await getTransactionByType(type)
     handdlerPromisses(get_transactions[0], get_transactions[1], 'GET_TRANSACTIONS')
@@ -26,11 +21,6 @@ btn_get_all_transaction.addEventListener('click', ()=> {
 const select_type = document.querySelector('#type')
 select_type.addEventListener('change', ()=> {
     shootGetTransactionByType(select_type.value)
-})
-
-const select_category = document.querySelector('#category_select_transaction')
-select_category.addEventListener('change', ()=> {
-    shootGetTransactionByCategory(select_category.value)
 })
 
 function handdlerPromisses(promisse, response, method){
@@ -53,6 +43,12 @@ function setDataInListAllTransactions(response) {
     const div_list = document.querySelector('.list_items')
     div_list.innerHTML ="";
     const ul = document.createElement('ul')
+    
+        let despesa=0
+        let receita=0
+        let cartao=0
+        let presente=0
+        let balance=0
 
     response.map(element => {
         const li = document.createElement('li')
@@ -62,6 +58,21 @@ function setDataInListAllTransactions(response) {
         const spanType = document.createElement('span')
         const spanValue = document.createElement('span')
 
+        switch (element.type) {
+            case 'despesa' :
+                despesa = Number(element.amount)
+            break;
+            case 'receita' :
+                receita = Number(element.amount)
+            break;
+            case 'cartão' :
+                cartao = Number(element.amount)
+            break;
+            case 'presente' :
+                presente = Number(element.amount)
+            break;
+        }
+        
         spanCategory.innerText = element.category.category_name
         spanType.innerText = element.type
         spanValue.innerText = `R$: ${element.amount}`
@@ -76,25 +87,38 @@ function setDataInListAllTransactions(response) {
         ul.appendChild(li)
     })
     div_list.appendChild(ul)
+
+    balance = (receita + presente) - (despesa + cartao)
+
+    const div_balance = document.querySelector('.transactions-balance')
+    div_balance.innerText = `Balanco das contas: R$ ${balance}`
 }
 
-function setCategoryDataInSelect(response){
-    if(response.length){
-        const select = document.querySelector('#category_select_transaction')
-        select.innerHTML = ""
-        response.forEach(element => {
-            const option = document.createElement('option');
-
-            option.value = element.id;
-            option.id = `${element.category_name}:${element.id}`
-            option.innerText =element.category_name
-
-            select.appendChild(option)
-        })
-    }
-}
 
 function autoStartFunctions(){
     shootGetTransactions()
-    shootGetCategorys()
 }autoStartFunctions()
+
+
+function calculateBalance(values, type){
+    let balance = 0
+    console.log(balance)
+
+    switch (type) {
+        case 'despesa' :
+            balance = - Number(values)
+        break;
+        case 'receita' :
+            balance = Number(values)
+        break;
+        case 'cartão' :
+            balance = - Number(values)
+        break;
+        case 'presente' :
+            balance = Number(values)
+        break;
+        default:
+            break;
+    }
+    return balance
+}
