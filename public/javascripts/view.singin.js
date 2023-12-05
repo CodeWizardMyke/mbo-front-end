e99997e5c589424e9c571c855be0c99c759336a1
CodeWizardMyke <maikewilliam.dev@gmail.com>
2023-12-04 23:44:49 -0300

    adicionando uso de jquery ao projeto download dos arquivos cdn para o projeto e melhorando codigo de login e cadastro

--- public/javascripts/view.singin.js
+++ public/javascripts/view.singin.js
@@ -1,71 +1,120 @@
-window.addEventListener('load', () => {
 
-    const formSingIn = document.querySelector('#singin_user');
-    formSingIn.addEventListener('submit', async (evt) => {
-        evt.preventDefault();
+$('#auth').submit( async (e) => {
+    e.preventDefault()
 
-        const formData = new FormData(formSingIn);
-        const formDataObj = {};
-        formData.forEach((value, key) => {
-            formDataObj[key] = value;
-        });
+    const email = $('#inEmail').val()
+    email ? '' : alert('email invalido!')
+    const pass = $('#inPass').val()
+    pass ? '' : alert('senha invalida!')
 
-        const opt = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json' 
-            },
-            body: JSON.stringify(formDataObj) 
-        };
-
-        const promisse = await fetch(`${urlBase}/user/login`, opt);
-        const response = await promisse.json();
-
-        ErrorHanddler(response, promisse)
-    });
-
-    function ErrorHanddler(response, promisse){
-        switch (promisse.status) {
-            case 200:
-                    LoginSucessiful(response)
-                    window.location = '/';
-                break;
-            case 400:
-                    SpanErrorUpdate()
-                    SpanErrorAdd(response)
-                break;
-            case 500:
-                alert('erro no servidor cod 500')
-                break;
-            default:
-                alert('error inesperado cod '+ promisse.status)
-                break;
-        }
+    const body = {
+        email:email,
+        password:pass,
     }
 
-    function LoginSucessiful(response){
-        const keepLoggedIn = document.querySelector('#keepLoggedIn')
-        if(keepLoggedIn.checked){
-            localStorage.setItem('token', response.token);
-            localStorage.setItem('user', JSON.stringify(response.user));
-        }else{
-            sessionStorage.setItem('token', response.token);
-            sessionStorage.setItem('user', JSON.stringify(response.user));
-        }
+    const options ={
+        method:'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(body),
+    };
+
+    const promisse = await fetch( `${urlBase}/user/login` ,options);
+    const response = await promisse.json();
+    
+    handlerResponses(response, promisse, true)
+})
+
+
+$('#singUp').submit( async (e) => {
+    e.preventDefault()
+
+    const fullname = $('#fullname').val()
+    fullname ? '' : alert('Nome de usuário invalido!')
+    
+    const email = $('#upEmail').val()
+    email ? '' : alert('email invalido!')
+    
+    const pass = $('#upPass').val()
+    pass ? '' : alert('senha invalida!')
+
+    const body = {
+        fullname:fullname,
+        email:email,
+        password:pass,
     }
+
+    const options ={
+        method:'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(body),
+    };
+
+    const promisse = await fetch( `${urlBase}/users` ,options);
+    const response = await promisse.json();
+    
+    handlerResponses(response, promisse, false)
+})
+
+function handlerResponses( response, promisse, key){
+    switch (promisse.status) {
+        case 200:
+            userLogedTrue(response);
+            break;
+        case 201:
+            userCreated(response);
+            break;
+        case 400:
+            errosFind( response, key );
+            break;
+        case 500:
+            alert('erro no servidor cod 500')
+            break;
+        default:
+            alert('error inesperado cod '+ promisse.status)
+        break;
+    }
+}
+    
+function userLogedTrue(response){
+    const isChecked = $('#authSinc').is(':checked');
+    
+    isChecked ? checkTrue() : checkedFalse() ;
+    
+    function checkTrue () { 
+        localStorage.setItem('token', response.token);
+        localStorage.setItem('user', JSON.stringify(response.user));
+    }
+
+    function checkedFalse () {
+        sessionStorage.setItem('token', response.token);
+        sessionStorage.setItem('user', JSON.stringify(response.user));
+    }
+    window.location = '/profile'
+}
+
+function userCreated (response) {
+    sessionStorage.setItem('token', response.token);
+    sessionStorage.setItem('user', JSON.stringify(response.user));
+    window.location = '/profile'
+}
+
+function errosFind(response, key){
 
     function SpanErrorAdd(response){
+        let stringErr = ''
+        key ? stringErr ='-error' : stringErr= '-up-error';
+
         response.forEach(error => {
-            const span = document.querySelector(`#${error.path}-error`);
-            span.innerHTML = error.msg;
+            const span = document.querySelector(`#${error.path}${stringErr}`);
+            span.innerHTML = error.msg.substring(0, 22);
             span.style = 'color:red;'
         })
-    }
+    }SpanErrorAdd(response);
+
     function SpanErrorUpdate(){
         const spanCathc = document.querySelectorAll('#singin_user span')
-        console.log(spanCathc)
         spanCathc.forEach(element => {
             element.innerHTML = ""
         });
-    }
-});
+    }SpanErrorUpdate();
+}
