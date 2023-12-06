0bef4d443466c8ad345699b74f34ae3849cf0b2b
CodeWizardMyke <maikewilliam.dev@gmail.com>
2023-12-05 23:51:57 -0300

    estilizando menu para navegação de usuario online

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,5 +1,4 @@
 const urlBase = 'https://mbo-backend-app.fly.dev';
-const urlProject = 'http://localhost:3000'
 
 const user = JSON.parse(localStorage.getItem('user')) || JSON.parse(sessionStorage.getItem('user'));
 const token = localStorage.getItem('token') || sessionStorage.getItem('token')
@@ -8,7 +7,7 @@ window.addEventListener('load', ()=> {
     //logout btn event
     $("#btn-logout").click( ()=> { userLogout() });
 
-    user ? userLogedTrue() : '';
+    user ? userLogedTrue() : userLogedFalse();
 
 })
 
@@ -19,8 +18,12 @@ function userLogout () {
     localStorage.removeItem('categorys')
 
     window.location = '/';
+    
 }
 
 function userLogedTrue () { 
-    $("#navigate-user").css('display','flex')
+    $(".sub-menu").css('display','flex')
+}
+function userLogedFalse() { 
+    $(".sub-menu").css('display','none')
 }
