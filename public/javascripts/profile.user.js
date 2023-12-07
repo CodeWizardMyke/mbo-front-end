3919193ae34cc1d203e8706dfad411785a2f082f
CodeWizardMyke <maikewilliam.dev@gmail.com>
2023-12-06 23:47:31 -0300

    concertado erro na linha 72

--- public/javascripts/profile.user.js
+++ public/javascripts/profile.user.js
@@ -69,7 +69,7 @@ $('#btn-transaction').click( () => {
 
 function getMonth(){
     function obterNomeMes(mes) {
-        const meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril','Maio', 'Junho', 'Julho', 'Agosto','Setembro', 'Outubro', 'Novembro', 'Dezembro';
+        const meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril','Maio', 'Junho', 'Julho', 'Agosto','Setembro', 'Outubro', 'Novembro', 'Dezembro'];
         if (mes >= 1 && mes <= 12) {return meses[mes - 1];} else {return 'Mês inválido';}
     }
     const data = new Date(); 
