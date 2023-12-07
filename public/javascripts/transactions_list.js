17b44b9b0c71eb633dc8b18bc9903be0df646b53
CodeWizardMyke <maikewilliam.dev@gmail.com>
2023-12-07 10:22:11 -0300

    resolvido todos bugs da pagina transaction list

--- public/javascripts/transactions_list.js
+++ public/javascripts/transactions_list.js
@@ -1,80 +1,85 @@
-const transactions = JSON.parse(transactions_data)
-transactions ? setDataInListAllTransactions(transactions) : GetTransactions() ;
+$(document).ready( async () => {
+    //ao carregar a pagina me traga todas as transações do usuario
+    getAllTransacitons();
 
-document.querySelector('#type').addEventListener('change', async ()=> {
-    const value = document.querySelector('#type').value
-    const get_transactions = value ? await getTransactionByType(value) : [];
+    //ao clicar em pegar todas transações me retornar a lista completa
+    $("#btn-all-transactions").click( ()=> { getAllTransacitons() } );
     
-    handdlerPromisses(get_transactions[0], get_transactions[1], 'GET_TRANSACTIONS')
-})
+    //ao clicar em tipo de categoria a transação pertence me filtre e me traga o resultado
+    $("#transaction-type").change( async () => {
+            const value = $('#transaction-type').val();
+            const {promisse, response} = await getTransactionByType(value);
+            handdlerPromisses(promisse, response);
+    });
+});
 
-document.querySelector('#bt_get_all_transactions').addEventListener('click', () => {
-    transactions ? setDataInListAllTransactions(transactions) : GetTransactions() ;
-})
+async function getAllTransacitons() {
+    const { promisse, response} = await getTransactions();
+    handdlerPromisses(promisse, response);
+    handdlerBalanceUser(response);
+};
 
-async function getBalance(){
-    const url = `${urlBase}/data/balance`
-    const opt = {
-        "method":"GET",
-        "headers":{
-            "Authorization":`Baerer ${token}`,
-        }
-    }
+function insertDataInList(response) { 
+    $(".list_items ul").empty();
 
-    const promisse = await fetch( url, opt );
-    return await promisse.json();
-}
+    if(response.length > 0 ){
+        response.map( element => {
+            $(".list_items ul").append(`
+            <li>
+                <a href="/profile/transaction/${element.id}">
+                    <div>
+                        <span>${element.category.category_name}</span>
+                        <span>${element.type}</span>
+                    </div>
+                    <span>R$: ${element.amount}</span>
+                </a>
+            </li>`) 
+        });
+    }else{
+        $(".list_items ul").append(`
+        <li>
+            <a>
+                <div>
+                    <span>nenhuma transação encontrada!</span>
+                    <span></span>
+                </div>
+                <span></span>
+            </a>
+        </li>`);
+    };
+};
 
-function handdlerPromisses(promisse, response, f){
-    const key = `${promisse.status}_${f}`
-
-    switch (key) {
-        case '200_GET_TRANSACTIONS':
-            setDataInListAllTransactions(response)
+function handdlerPromisses(promisse, response){
+    switch (promisse.status) {
+        case 200:
+            insertDataInList(response);
             break;
+        case 400:
+            alert("Error pagina não encontrada!");
+        case 500:
+            alert("Erro no lado do servidor!");
         default:
-            console.log(`Error inesperado cod: ${response.status}`)
+            alert(`Erro inesperado cod:_${promisse.status}`);
             break;
-    }
-}
+    };
+};
 
-function setDataInListAllTransactions(response) {
-    const div_list = document.querySelector('.list_items')
-    div_list.innerHTML ="";
-    const ul = document.createElement('ul')
+function handdlerBalanceUser(response){
+    let expense = 0
+    let revenue = 0
 
     response.map(element => {
-        const li = document.createElement('li')
-        const anchor = document.createElement('a')
-        const div = document.createElement('div')
-        const spanCategory = document.createElement('span')
-        const spanType = document.createElement('span')
-        const spanValue = document.createElement('span')
-
-        spanCategory.innerText = element.category.category_name
-        spanType.innerText = element.type
-        spanValue.innerText = `R$: ${element.amount}`
-        
-        anchor.href= `/profile/transaction/${element.id}`
-
-        anchor.appendChild(div)
-        anchor.appendChild(spanValue)
-        div.appendChild(spanCategory)
-        div.appendChild(spanType)
-        li.appendChild(anchor)
-        ul.appendChild(li)
+        if(element.type === 'despesa' || element.type === 'card'){
+            expense += Number(element.amount);
+        }else{
+            revenue += Number(element.amount);
+        }
     })
-    div_list.appendChild(ul)
-}
+    let balance = revenue - expense;
+    let clasName = balance > 0 ? 'positive' : 'negative';
 
-
-function insertDomBalance(){
-    getBalance()
-        .then(data => {
-            const div_balance = document.querySelector('.transactions-balance')
-            div_balance.innerText = `Balanco das contas: R$ ${data.balance}`
-        })
-        .catch(err => console.log(err))
-
-}insertDomBalance()
-
+    $(".transactions-balance").empty()
+    $(".transactions-balance").append(`
+        <span>Balanco das contas: R$ <span class="${clasName}">${balance}</span></span>
+    `)
+};
