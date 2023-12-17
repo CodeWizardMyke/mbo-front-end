ca29d333f699f003729fdb0f88a9c407a7fe54d3
CodeWizardMyke <maikewilliam.dev@gmail.com>
2023-12-17 01:42:52 -0300

    mudanças na estrutura html e css do formulario

--- public/javascripts/profile.events.js
+++ public/javascripts/profile.events.js
@@ -1,9 +1,10 @@
 $(document).ready( ()=>{
     //pegar as categorias para serem usadas na criação de uma nova transação
-    $('#btn-transaction').click( async () => {
+    $(document).ready( async () => {
         const {promisse, response} = await getCategorys()
         handdlerTransaction( promisse, response, 'categorys' )
     } );
+    
     //postar nova transação no banco de dados
     $("#form-transaction").submit( async (evt) => {
         evt.preventDefault();
@@ -29,8 +30,43 @@ function handdlerTransaction(promisse, response, when){
 }
 
 function populateSelectCategory(response) {
-    $("#categorys-t:not(:first)").remove()
+    $("#span_list ul").empty();
     response.forEach(element => {
-        $("#categorys-t").append(`<option value="${element.id}">${element.category_name}</option>`)
+        let listItem = $(`
+            <li>${element.category_name}<input type="text" value="${element.id}"></li>
+        `);
+        listItem.click(() => { clickHandler(element.category_name, element.id) });
+        $("#span_list ul").append(listItem);
     });
+
+    function clickHandler(category_name, id) {
+        $("#category_name").empty();
+        $("#category_name").append(`${category_name}`);
+        $("#category_value").append(`${id}`);
+    }
+}
+
+
+$('#span_transactions_list ul li').click(function() {
+    const selectedValue = $(this).find('input').val();
+    const selectedText = $(this).text();
+    $('#installments_line').empty()
+
+    if(selectedValue == 'cartão'){
+        tableInsertLine()
+    }
+
+    $('#type_transaction').text(selectedText);
+    $('#type_tr').val(selectedValue);
+});
+
+function tableInsertLine(){
+    $('#installments_line').append(`
+        <td>
+            <label for="installments">Parcelas</label>
+        </td>
+        <td>
+            <input type="number" name="installments" class="input-group-text" value="0">
+        </td>
+    `)
 }
