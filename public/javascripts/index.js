0bf7bb1723974b1393b3713ce7973114e19ce76b
CodeWizardMyke <maikewilliam.dev@gmail.com>
2023-12-20 23:39:11 -0300

    melhorando responsividade do chat

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,4 +1,4 @@
-const urlBase = 'http://localhost:1515' //'https://mbo-back-end.fly.dev';
+const urlBase = 'http://localhost:1515'  //'https://mbo-back-end.fly.dev';
 
 const user = JSON.parse(localStorage.getItem('user')) || JSON.parse(sessionStorage.getItem('user'));
 const token = localStorage.getItem('token') || sessionStorage.getItem('token')
